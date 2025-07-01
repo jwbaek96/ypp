e9994b18e3278397a0167e3194527ab0a1c2b578
jwbaek96 <jwbaek96@gmail.com>
2025-07-01 18:21:39 +0900

    .

---
+++ redirect.js
@@ -0,0 +1,34 @@
+// redirect.js
+function autoRedirect() {
+    const hostname = window.location.hostname;
+    const pathname = window.location.pathname;
+    console.log('현재 hostname:', hostname);
+    console.log('현재 pathname:', pathname);
+    
+    // 배포 도메인 체크 (로컬 테스트용으로 5500 포트 포함)
+    const isGitHubPages = hostname.includes('github.io');
+    const isProductionDomain = hostname.includes('ypp.co.kr');
+    const isLocalTest = hostname.includes('127.0.0.1') && window.location.port === '5500'; // 로컬 테스트용
+    
+    const isLockWebsite = pathname.includes('renewallock.html');
+    
+    console.log('GitHub Pages:', isGitHubPages);
+    console.log('Production:', isProductionDomain);
+    console.log('Local Test:', isLocalTest);
+    console.log('Is Lock Website:', isLockWebsite);
+    
+    // 배포 환경이면서 lockwebsite가 아닌 경우 리다이렉션
+    if ((
+        isGitHubPages 
+        || 
+        isProductionDomain 
+        // || 
+        // isLocalTest
+        ) && !isLockWebsite) {
+        console.log('리다이렉션 실행: renewallock.html로 이동');
+        window.location.replace('./renewallock.html');
+    }
+}
+
+// 즉시 실행
+autoRedirect();
